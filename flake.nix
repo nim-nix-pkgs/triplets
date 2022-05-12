e933c4e2b87d2f5325f22a4c35262b7a239501ae
hugosenari <hugosenari@gmail.com>
2022-05-12 11:58:24 -0300

    chore: re index triplets

--- flake.nix
+++ flake.nix
@@ -7,6 +7,94 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."triplets-main".dir   = "main";
+  inputs."triplets-main".owner = "nim-nix-pkgs";
+  inputs."triplets-main".ref   = "master";
+  inputs."triplets-main".repo  = "triplets";
+  inputs."triplets-main".type  = "github";
+  inputs."triplets-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."triplets-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."triplets-master".dir   = "master";
+  inputs."triplets-master".owner = "nim-nix-pkgs";
+  inputs."triplets-master".ref   = "master";
+  inputs."triplets-master".repo  = "triplets";
+  inputs."triplets-master".type  = "github";
+  inputs."triplets-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."triplets-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."triplets-0_0_1".dir   = "0_0_1";
+  inputs."triplets-0_0_1".owner = "nim-nix-pkgs";
+  inputs."triplets-0_0_1".ref   = "master";
+  inputs."triplets-0_0_1".repo  = "triplets";
+  inputs."triplets-0_0_1".type  = "github";
+  inputs."triplets-0_0_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."triplets-0_0_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."triplets-0_0_2".dir   = "0_0_2";
+  inputs."triplets-0_0_2".owner = "nim-nix-pkgs";
+  inputs."triplets-0_0_2".ref   = "master";
+  inputs."triplets-0_0_2".repo  = "triplets";
+  inputs."triplets-0_0_2".type  = "github";
+  inputs."triplets-0_0_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."triplets-0_0_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."triplets-0_0_3".dir   = "0_0_3";
+  inputs."triplets-0_0_3".owner = "nim-nix-pkgs";
+  inputs."triplets-0_0_3".ref   = "master";
+  inputs."triplets-0_0_3".repo  = "triplets";
+  inputs."triplets-0_0_3".type  = "github";
+  inputs."triplets-0_0_3".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."triplets-0_0_3".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."triplets-0_0_4".dir   = "0_0_4";
+  inputs."triplets-0_0_4".owner = "nim-nix-pkgs";
+  inputs."triplets-0_0_4".ref   = "master";
+  inputs."triplets-0_0_4".repo  = "triplets";
+  inputs."triplets-0_0_4".type  = "github";
+  inputs."triplets-0_0_4".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."triplets-0_0_4".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."triplets-0_0_5".dir   = "0_0_5";
+  inputs."triplets-0_0_5".owner = "nim-nix-pkgs";
+  inputs."triplets-0_0_5".ref   = "master";
+  inputs."triplets-0_0_5".repo  = "triplets";
+  inputs."triplets-0_0_5".type  = "github";
+  inputs."triplets-0_0_5".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."triplets-0_0_5".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."triplets-0_0_6".dir   = "0_0_6";
+  inputs."triplets-0_0_6".owner = "nim-nix-pkgs";
+  inputs."triplets-0_0_6".ref   = "master";
+  inputs."triplets-0_0_6".repo  = "triplets";
+  inputs."triplets-0_0_6".type  = "github";
+  inputs."triplets-0_0_6".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."triplets-0_0_6".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."triplets-0_0_7".dir   = "0_0_7";
+  inputs."triplets-0_0_7".owner = "nim-nix-pkgs";
+  inputs."triplets-0_0_7".ref   = "master";
+  inputs."triplets-0_0_7".repo  = "triplets";
+  inputs."triplets-0_0_7".type  = "github";
+  inputs."triplets-0_0_7".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."triplets-0_0_7".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."triplets-0_1_0".dir   = "0_1_0";
+  inputs."triplets-0_1_0".owner = "nim-nix-pkgs";
+  inputs."triplets-0_1_0".ref   = "master";
+  inputs."triplets-0_1_0".repo  = "triplets";
+  inputs."triplets-0_1_0".type  = "github";
+  inputs."triplets-0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."triplets-0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."triplets-0_1_1".dir   = "0_1_1";
+  inputs."triplets-0_1_1".owner = "nim-nix-pkgs";
+  inputs."triplets-0_1_1".ref   = "master";
+  inputs."triplets-0_1_1".repo  = "triplets";
+  inputs."triplets-0_1_1".type  = "github";
+  inputs."triplets-0_1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."triplets-0_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
